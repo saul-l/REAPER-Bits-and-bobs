74e906d7b987b20922dd2caec746ce846cf47729
Demoji-x <sauli.lehtinen@gmail.com>
2022-03-15 10:36:48 +0200

    Update NextVariation.lua
    added proper about info

--- NextVariation/NextVariation.lua
+++ NextVariation/NextVariation.lua
@@ -1,10 +1,28 @@
 desc: NextVariation
-tags: Filther
 version: 1.0
 author: Sauli Lehtinen / Demoji
 about:
-  Reascript action for finding next variation in audio item.
-  Uses built-in transient detection, which can be configured under action "Transient detection sensitivity/threshold: Adjust..." 
+  Sets content of selected audio item to next variation found. Perfect for sound designers working with recordings containing multiple variations in single file.
+
+  Uses built-in transient detection, which can be configured with action "Transient decetion sensitivity/threshold: Adjust..."
+
+  Usage example:
+  - Add audio file containing multiple variations of sound into timeline
+  - Trim item to contain only first variation
+  - Select item and run script
+  - Item should now have next variation
+
+  1.0 release highlights:
+  - Supports multiple selected items at a same time
+  - Uses separate tracks for item manipulation, so shouldn't mess with unselected track content
+  - Loops through item content, if Loop source is enabled
+  - Preserves item fade information
+  - Smarter UI update disabling, which gives huge performance boost
+
+  Known issues and workaround:
+  - Built-in transient detection is not content aware and therefor you often have to choose between getting false positives or not finding the next variation. False positives are not really a big issue, since you can just run the script again (using keyboard shortcut strongly recommended). Sensitivity 35%, Threshold -24db settings seem to work fine (= a bit too false positive happy, but not missing anything) with most audio content. 
+changelog:
+  First public release
 
   newItemList = {}
   
